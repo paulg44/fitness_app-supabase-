e97b22fb05a4ce40058a899db14f59e07ce8a952
paulg44 <paulgarton1984@hotmail.com>
2023-12-31 20:07:46 +0000

    add about page and render test

--- fitness-app/src/App.tsx
+++ fitness-app/src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 
 // Pages
 import Homepage from "./Pages/Homepage/Homepage";
+import About from "./Pages/About/About";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </>
   );
