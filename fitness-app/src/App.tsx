d981445b7304a31218da67ab42f0a01b76f49930
paulg44 <paulgarton1984@hotmail.com>
2024-01-02 20:12:32 +0000

    add basic navbar for easy navigation to pages

--- fitness-app/src/App.tsx
+++ fitness-app/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // Pages
+import Navbar from "./Components/Navbar/Navbar";
 import Homepage from "./Pages/Homepage/Homepage";
 import About from "./Pages/About/About";
 import EnterLogForm from "./Components/EnterLogForm/EnterLogForm";
@@ -10,6 +11,7 @@ import EnterLogForm from "./Components/EnterLogForm/EnterLogForm";
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<About />}></Route>
---
+++ fitness-app/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+// Navbar Component
+
+import { Nav } from "react-bootstrap";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <Nav>
+      <div className="logo">LOGO</div>
+      <div className="links">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </Nav>
+  );
+}
+
+export default Navbar;
