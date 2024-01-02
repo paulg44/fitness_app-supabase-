cb3c8e7b87582743be94edb6a8f087e7405e24fa
paulg44 <paulgarton1984@hotmail.com>
2024-01-02 19:54:04 +0000

    add basic form with limited fields

--- fitness-app/src/Components/EnterLogForm/EnterLogForm.tsx
+++ fitness-app/src/Components/EnterLogForm/EnterLogForm.tsx
@@ -1,7 +1,38 @@
 // Enter Log Form Component
 
+import { Form, Button } from "react-bootstrap";
+
 function EnterLogForm() {
-  return <div>Log form will go here</div>;
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formEnterDate">
+        <Form.Label>Enter Distance</Form.Label>
+        <Form.Control type="text" placeholder="Enter Date" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formEnterDistance">
+        <Form.Label>Enter Distance</Form.Label>
+        <Form.Control type="text" placeholder="Enter Distance" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formEnterDuration">
+        <Form.Label>Enter Activity Duration</Form.Label>
+        <Form.Control type="text" placeholder="Enter Duration" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formEnterSurface">
+        <Form.Label>Select Surface</Form.Label>
+        <Form.Select aria-label="Select Surface">
+          <option></option>
+          <option value="1">Road</option>
+          <option value="2">Track</option>
+          <option value="3">Trail</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formEnterDesc">
+        <Form.Label>Description</Form.Label>
+        <Form.Control type="text" placeholder="" />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
 }
 
 export default EnterLogForm;
