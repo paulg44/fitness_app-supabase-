2ba69d7b8abe86a58cea93cef23fa5ec4e6b48fc
paulg44 <paulgarton1984@hotmail.com>
2024-01-04 20:21:28 +0000

    add use states and change functions to register form

--- fitness-app/src/Components/RegisterForm/RegisterForm.tsx
+++ fitness-app/src/Components/RegisterForm/RegisterForm.tsx
@@ -2,21 +2,61 @@
 
 import { Button, Form } from "react-bootstrap";
 import "./RegisterForm.css";
+import { ChangeEvent, useState } from "react";
 
 function RegisterForm() {
+  // States
+  const [createUsername, setCreateUsername] = useState("");
+  const [enterEmail, setEnterEmail] = useState("");
+  const [createPassword, setCreatePassword] = useState("");
+
+  // Functions
+  // Create Username
+  function handleCreateUsername(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setCreateUsername(e.target.value);
+    console.log(createUsername);
+  }
+
+  // Enter Email
+  function handleEnterEmail(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setEnterEmail(e.target.value);
+    console.log(enterEmail);
+  }
+
+  // Create Password
+  function handleCreatePassword(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setCreatePassword(e.target.value);
+    console.log(createPassword);
+  }
+
   return (
     <Form>
       <Form.Group controlId="createUsername">
         <Form.Label>Create Username</Form.Label>
-        <Form.Control type="text" placeholder="username"></Form.Control>
+        <Form.Control
+          type="text"
+          placeholder="username"
+          onChange={handleCreateUsername}
+        ></Form.Control>
       </Form.Group>
       <Form.Group controlId="enterEmail">
         <Form.Label>Enter Email</Form.Label>
-        <Form.Control type="email" placeholder="email"></Form.Control>
+        <Form.Control
+          type="email"
+          placeholder="email"
+          onChange={handleEnterEmail}
+        ></Form.Control>
       </Form.Group>
       <Form.Group controlId="createPassword">
         <Form.Label>Create Password</Form.Label>
-        <Form.Control type="password" placeholder="password"></Form.Control>
+        <Form.Control
+          type="password"
+          placeholder="password"
+          onChange={handleCreatePassword}
+        ></Form.Control>
       </Form.Group>
       <Button type="submit">Register</Button>
     </Form>
